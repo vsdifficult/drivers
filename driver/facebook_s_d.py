920a0a864a5d0872060331d08d8f5eee7ab1fed5
DIfficult <101355829+vsdifficult@users.noreply.github.com>
2023-08-17 16:17:21 +0300

    Create facebook_s_d.py

---
+++ driver/facebook_s_d.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.keys import Keys
+
+class facebookP(): 
+
+    def __init__(self, page) -> None:
+        self.page = page  
+    
+    def commentsf(self): 
+        pass
